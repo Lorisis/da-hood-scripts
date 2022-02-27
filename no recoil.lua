9e09f6d65d1ed97cb585764eb5a5aeb7e8eaaea9
Loris <97990609+Lorisis@users.noreply.github.com>
2022-02-27 02:24:35 -0500

    Create no recoil.lua

---
+++ no recoil.lua
@@ -0,0 +1,5 @@
+local CurrentFocus = game:GetService("Workspace").CurrentCamera.CFrame
+game:GetService("Workspace").CurrentCamera:Destroy()
+local Instance = Instance.new("Camera", game:GetService("Workspace"))
+Instance.CameraSubject = game:GetService("Players").LocalPlayer.Character.Humanoid
+Instance.CameraType = Enum.CameraType.Custom
